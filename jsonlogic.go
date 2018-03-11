b64ddd1299f72222e69648db97c5f8bd7b89174e
Nathan Hazout <nathanh@il.ibm.com>
2018-03-11 17:09:37 +0200

    prevent crash on empty array
    Signed-off-by: Nathan Hazout <nathanh@il.ibm.com>

--- jsonlogic.go
+++ jsonlogic.go
@@ -74,12 +74,17 @@ func applyInterfaces(inputs ...interface{}) interface{} {
 				switch value.(type) {
 				case []interface{}: // An array of values
 					valuearray := value.([]interface{})
-					value1 := applyInterfaces(valuearray[0], data)
-					var value2 interface{}
-					if len(valuearray) > 1 {
-						value2 = applyInterfaces(valuearray[1], data)
+					if len(valuearray) > 0 {
+						value1 := applyInterfaces(valuearray[0], data)
+						var value2 interface{}
+						if len(valuearray) > 1 {
+							value2 = applyInterfaces(valuearray[1], data)
+						}
+						return dataLookup(data, value1, value2)
 					}
-					return dataLookup(data, value1, value2)
+					//TODO: Expected behavior for empty array?
+					return false
+
 				default: // A single value
 					return dataLookup(data, applyInterfaces(value, data), nil)
 				}
