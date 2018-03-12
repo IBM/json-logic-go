77bfa0f2e63e028ae0822f6aba317b250cc1ad9d
waels <wael.shama@ibm.com>
2018-03-12 15:56:12 +0200

    WI 50203: implement `merge` operator

--- op_merge.go
+++ op_merge.go
@@ -1,6 +1,26 @@
 package jsonlogic
 
-func opMerge(value interface{}, data interface{}) interface{} {
-	// return true
-	return nil
+func opMerge(value interface{}, data interface{}) []interface{} {
+	var result []interface{}
+	switch value.(type) {
+	case []interface{}:
+		valuearray := value.([]interface{})
+		if len(valuearray) == 0 {
+			result = value.([]interface{})
+		}
+		for _, val := range valuearray {
+			switch val.(type) {
+			case []interface{}:
+				result = append(result, opMerge(val, data)...)
+			case interface{}:
+				result = append(result, val)
+			}
+		}
+	default:
+		if value != nil {
+			result = append(result, value)
+		}
+	}
+
+	return result
 }
