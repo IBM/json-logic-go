7c26ae1e252ade1cbb074098429c89b24a247193
waels <wael.shama@ibm.com>
2018-03-12 17:12:56 +0200

    WI 50206: implement `cat` operator

--- op_cat.go
+++ op_cat.go
@@ -1,5 +1,31 @@
 package jsonlogic
 
-func opCat(value interface{}, data interface{}) interface{} {
-	return nil
+import (
+	"strconv"
+)
+
+func opCat(value interface{}, data interface{}) string {
+	var result string
+	switch value.(type) {
+	case []interface{}:
+		valuearray := value.([]interface{})
+		for _, val := range valuearray {
+			switch val.(type) {
+			case string:
+				result += val.(string)
+			case int:
+				result += strconv.Itoa((val.(int)))
+			case float64:
+				result += strconv.Itoa((int(val.(float64))))
+			default:
+				result += applyInterfaces(val, data).(string)
+			}
+		}
+	default:
+		if value != nil {
+			result = value.(string)
+		}
+	}
+	return result
+
 }
