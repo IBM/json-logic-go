0754a34bd7d0596e2c770d38cb26eed21f2878c9
waels <wael.shama@ibm.com>
2018-03-13 14:45:28 +0200

    WI 50197:  refactor `map` operator

--- op_map.go
+++ op_map.go
@@ -2,18 +2,34 @@ package jsonlogic
 
 func opMap(value interface{}, data interface{}) []interface{} {
 	valuearray := value.([]interface{})
-	arrayName := valuearray[0].(map[string]interface{})["var"]
+	array := applyInterfaces(valuearray[0], data)
 	operation := valuearray[1].(map[string]interface{})
 	var result []interface{}
+
 	if data != nil {
 		mapData := data.(map[string]interface{})
-		array := mapData[arrayName.(string)]
-		for _, val := range array.([]interface{}) {
-			res := applyInterfaces(operation, val)
-			result = append(result, res)
+		switch array.(type) {
+		case string: //get from Data
+			arrays := mapData[array.(string)]
+			for _, val := range arrays.([]interface{}) {
+				res := applyInterfaces(operation, val)
+				result = append(result, res)
+			}
+		default: // Hardcoded
+			for _, val := range array.([]interface{}) {
+				res := applyInterfaces(operation, val)
+				result = append(result, res)
+			}
 		}
 	} else {
-		result = []interface{}{}
+		if array == nil || len(array.([]interface{})) == 0 {
+			result = []interface{}{}
+		} else { // Hardcoded
+			for _, val := range array.([]interface{}) {
+				res := applyInterfaces(operation, val)
+				result = append(result, res)
+			}
+		}
 	}
 	return result
 }
