a4200e8c2159bcb1052dd1f0b2901ec4cb70fff4
waels <wael.shama@ibm.com>
2018-03-13 15:08:19 +0200

    WI 50197 : refactor `map` operator

--- op_map.go
+++ op_map.go
@@ -6,29 +6,12 @@ func opMap(value interface{}, data interface{}) []interface{} {
 	operation := valuearray[1].(map[string]interface{})
 	var result []interface{}
 
-	if data != nil {
-		mapData := data.(map[string]interface{})
-		switch array.(type) {
-		case string: //get from Data
-			arrays := mapData[array.(string)]
-			for _, val := range arrays.([]interface{}) {
-				res := applyInterfaces(operation, val)
-				result = append(result, res)
-			}
-		default: // Hardcoded
-			for _, val := range array.([]interface{}) {
-				res := applyInterfaces(operation, val)
-				result = append(result, res)
-			}
-		}
+	if array == nil || len(array.([]interface{})) == 0 {
+		result = []interface{}{}
 	} else {
-		if array == nil || len(array.([]interface{})) == 0 {
-			result = []interface{}{}
-		} else { // Hardcoded
-			for _, val := range array.([]interface{}) {
-				res := applyInterfaces(operation, val)
-				result = append(result, res)
-			}
+		for _, val := range array.([]interface{}) {
+			res := applyInterfaces(operation, val)
+			result = append(result, res)
 		}
 	}
 	return result
