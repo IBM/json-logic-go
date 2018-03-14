1e51b482f293e4de7cebf201161d4b94f23767e0
waels <wael.shama@ibm.com>
2018-03-14 10:47:10 +0200

    WI 50199 : refactor reduce operator

--- op_reduce.go
+++ op_reduce.go
@@ -5,20 +5,20 @@ func opReduce(value interface{}, data interface{}) float64 {
 	valuearray := value.([]interface{})
 	array := applyInterfaces(valuearray[0], data)
 	operation := valuearray[1]
-	accumulator := valuearray[2]
+	accumulator := valuearray[2].(float64)
 
 	if array == nil {
-		return 0
+		return accumulator
 	}
-	var dat = map[string]interface{}{}
 
+	var dat = map[string]interface{}{}
 	for _, val := range array.([]interface{}) {
 		//  "current" : this element of the array
 		// "accumulator" : progress so far, or the initial value
 		dat["current"] = val
 		dat["accumulator"] = accumulator
-		accumulator = applyInterfaces(operation, dat)
+		accumulator = applyInterfaces(operation, dat).(float64)
 	}
 
-	return accumulator.(float64)
+	return accumulator
 }
