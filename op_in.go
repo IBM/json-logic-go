06a1a1c99aef343c3c91feec6e0c469e48eedb31
Nathan Hazout <nathanh@il.ibm.com>
2018-03-18 11:21:47 +0200

    wi 50205 “in” stop assuming everything is string
    Signed-off-by: Nathan Hazout <nathanh@il.ibm.com>

--- op_in.go
+++ op_in.go
@@ -8,15 +8,9 @@ func opIn(value interface{}, data interface{}) (interface{}, error) {
 	valuearray := value.([]interface{})
 	var needle, haystack interface{}
 	var err error
-	//check needle type
-	switch valuearray[0].(type) {
-	case string:
-		needle = valuearray[0].(string)
-	default:
-		needle, err = applyInterfaces(valuearray[0], data)
-		if err != nil {
-			return nil, err
-		}
+	needle, err = applyInterfaces(valuearray[0], data)
+	if err != nil {
+		return nil, err
 	}
 
 	haystack, err = applyInterfaces(valuearray[1], data)
@@ -29,23 +23,9 @@ func opIn(value interface{}, data interface{}) (interface{}, error) {
 		return strings.Contains(haystack.(string), needle.(string)), nil
 	case []interface{}:
 		for _, word := range haystack.([]interface{}) {
-			switch word.(type) {
-			case string:
-				if needle.(string) == word.(string) {
-					return true, nil
-				}
-			// TODO: What if it is a numerical value or something else that is not a string?
-			default:
-				wordString, err := applyInterfaces(word, data)
-				if err != nil {
-					return nil, err
-				}
-				if needle.(string) == wordString.(string) {
-					return true, nil
-				}
-				return false, nil
+			if needle == word {
+				return true, nil
 			}
-
 		}
 	default:
 		return false, nil
