e4d359226d3975559345fb81c4394c19dc48be4f
Nathan Hazout <nathanh@il.ibm.com>
2018-03-21 16:46:04 +0200

    fizzbuzz test
    Signed-off-by: Nathan Hazout <nathanh@il.ibm.com>

--- jsonlogic_test.go
+++ jsonlogic_test.go
@@ -400,6 +400,22 @@ func TestMerge(t *testing.T) {
 	assert.Nil(t, result)
 }
 
+func TestFizzBuzz(t *testing.T) {
+	var result interface{}
+	var err error
+
+	result, err = Apply(`{"map":[[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30],{"if":[{"==":[{"%":[{"var":""},15]},0]},"fizzbuzz",{"==":[{"%":[{"var":""},3]},0]},"fizz",{"==":[{"%":[{"var":""},5]},0]},"buzz",{"var":""}]}]}`)
+	expected, _ := stringToInterface(`[1,2,"fizz",4,"buzz","fizz",7,8,"fizz","buzz",11,"fizz",13,14,"fizzbuzz",16,17,"fizz",19,"buzz","fizz",22,23,"fizz","buzz",26,"fizz",28,29,"fizzbuzz"]`)
+	assert.NoError(t, err)
+	assert.Equal(t, expected, result)
+
+	result, err = Apply(`{"map":[{"var":"a"},{"if":[{"==":[{"%":[{"var":""},15]},0]},"fizzbuzz",{"==":[{"%":[{"var":""},3]},0]},"fizz",{"==":[{"%":[{"var":""},5]},0]},"buzz",{"var":""}]}]}`,
+		`{"a":[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]}`)
+	expected, _ = stringToInterface(`[1,2,"fizz",4,"buzz","fizz",7,8,"fizz","buzz",11,"fizz",13,14,"fizzbuzz",16,17,"fizz",19,"buzz","fizz",22,23,"fizz","buzz",26,"fizz",28,29,"fizzbuzz"]`)
+	assert.NoError(t, err)
+	assert.Equal(t, expected, result)
+}
+
 // Helper functions
 func getJSON(url string, target interface{}) error {
 	var client = http.Client{Timeout: 100 * time.Second}
