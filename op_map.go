28882f5a7be548be46371263d37c453e2a1e6c29
waels <wael.shama@ibm.com>
2018-03-21 14:22:49 +0200

    refactor map

--- op_map.go
+++ op_map.go
@@ -2,18 +2,19 @@ package jsonlogic
 
 func opMap(value interface{}, data interface{}) (interface{}, error) {
 	valuearray := value.([]interface{})
+	if len(valuearray) == 0 {
+		return []interface{}{}, nil
+	}
 	array, err := applyInterfaces(valuearray[0], data)
 	if err != nil {
 		return nil, err
 	}
-	var result []interface{}
-
-	if err != nil {
-		result = []interface{}{}
-		return result, err
+	var operation interface{}
+	if len(valuearray) == 2 {
+		operation = valuearray[1].(map[string]interface{})
 	}
-	operation := valuearray[1].(map[string]interface{})
 
+	var result []interface{}
 	if array == nil || len(array.([]interface{})) == 0 {
 		result = []interface{}{}
 	} else {
