da9480bed05c94104daf64688b186836d7896f6a
waels <wael.shama@ibm.com>
2018-03-21 12:38:39 +0200

    fix min and max to return proper error

--- op_min_max.go
+++ op_min_max.go
@@ -1,20 +1,24 @@
 package jsonlogic
 
 func opMax(value interface{}, data interface{}) (interface{}, error) {
-	_, max := findMinMax(value, data)
-
+	_, max, err := findMinMax(value, data)
+	if err != nil {
+		return nil, err
+	}
 	return max, nil
 }
 
 func opMin(value interface{}, data interface{}) (interface{}, error) {
-	min, _ := findMinMax(value, data)
-
+	min, _, err := findMinMax(value, data)
+	if err != nil {
+		return nil, err
+	}
 	return min, nil
 }
 
-func findMinMax(value interface{}, data interface{}) (min interface{}, max interface{}) {
+func findMinMax(value interface{}, data interface{}) (min interface{}, max interface{}, err error) {
 	if value == nil {
-		return 0, 0
+		return 0, 0, nil
 	}
 
 	switch value.(type) {
@@ -22,15 +26,15 @@ func findMinMax(value interface{}, data interface{}) (min interface{}, max inter
 		valuearray := value.([]interface{})
 
 		if len(valuearray) == 0 {
-			return nil, nil
+			return nil, nil, nil
 		}
 
 		val, err := applyInterfaces(valuearray[0], data)
 		if err != nil {
-			return nil, err
+			return nil, nil, err
 		}
 		if !isNumeric(val) {
-			return nil, nil
+			return nil, nil, nil
 		}
 
 		min := interfaceToFloat(val)
@@ -41,10 +45,10 @@ func findMinMax(value interface{}, data interface{}) (min interface{}, max inter
 		for i := 1; i < len(valuearray); i++ {
 			val, err = applyInterfaces(valuearray[i], data)
 			if err != nil {
-				return nil, err
+				return nil, nil, err
 			}
 			if !isNumeric(val) {
-				return nil, nil
+				return nil, nil, nil
 			}
 
 			floatVal = interfaceToFloat(val)
@@ -56,19 +60,19 @@ func findMinMax(value interface{}, data interface{}) (min interface{}, max inter
 			}
 		}
 
-		return min, max
+		return min, max, nil
 
 	default: // A single value
 		val, err := applyInterfaces(value, data)
 		if err != nil {
-			return nil, err
+			return nil, nil, err
 		}
 		if !isNumeric(val) {
-			return nil, nil
+			return nil, nil, nil
 		}
 
 		floatVal := interfaceToFloat(val)
 
-		return floatVal, floatVal
+		return floatVal, floatVal, nil
 	}
 }
